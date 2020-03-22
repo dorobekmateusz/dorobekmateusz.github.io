ace5c2cb6646be102a2979464ab5ceceb02db15e
SaxMan96 <mateuszdorobek@gmail.com>
2020-03-23 00:17:59 +0100

    MathJax fixes new verion assets

--- assets/js/MathJaxLocal.js
+++ assets/js/MathJaxLocal.js
@@ -1,7 +1,7 @@
 MathJax.Hub.Config({
   jax: ["input/TeX", "output/HTML-CSS"],
   tex2jax: {
-    inlineMath: [['$', '$']],
+    inlineMath: [['$', '$'], ["\\(","\\)"]],
     displayMath: [['$$', '$$'],['\\[','\\]']],
     processEscapes: true,
     skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
@@ -9,7 +9,7 @@ MathJax.Hub.Config({
   messageStyle: "none",
   "HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"] },
   TeX: {
-    equationNumbers: { autoNumber: "AMS" },
+    equationNumbers: { autoNumber: "all" },
     extensions: ["AMSmath.js", "AMSsymbols.js","AMScd.js"],
     TagSide: "left",
     Macros: {
